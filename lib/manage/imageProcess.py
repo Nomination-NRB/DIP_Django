d6b919fbf0e4192abf9e5a8fa90a841114d3ae21
ZEH0 <1335819001@qq.com>
2022-05-19 12:37:23 +0800

    [feat] 获取直方图字典 获取图片类型

---
+++ lib/manage/imageProcess.py
@@ -0,0 +1,50 @@
+import cv2
+
+from urllib import parse
+
+
+def imageResize(x, y, filePath):
+    filePath = parse.unquote(filePath)
+    img = cv2.imread(filePath)
+    x = float(x)
+    y = float(y)
+    out = cv2.resize(img, None, fx=x, fy=y, interpolation=cv2.INTER_LINEAR)
+
+def judge_img_type(img):
+    """
+    判断图片类型
+
+    参数:
+        img: 图片
+    返回:
+        imgType: 图片类型:rgb/gray string
+    """
+    img0 = img[:, :, 0]
+    img1 = img[:, :, 1]
+    img2 = img[:, :, 2]
+    if (img0 == img1).all() and (img0 == img2).all():
+        imgType = 'gray'
+    else:
+        imgType = 'rgb'
+    return imgType
+
+def get_hist_dict(filePath):
+    """
+    获取图片直方图
+
+    参数:
+        filepath: 图片路径
+    返回:
+        hist_dict: 图片直方图:dict
+    """
+    filepath = parse.unquote(filePath)
+    img = cv2.imread(filepath)
+    imgType = judge_img_type(img)
+    hist_dict={'r':[],'g':[],'b':[],'gray':[]}
+    if imgType == 'gray':
+        hist_dict['gray'] = cv2.calcHist([img], [0], None, [256], [0, 256]).reshape(-1)
+    else:
+        hist_dict['r'] = cv2.calcHist([img], [0], None, [256], [0, 256]).reshape(-1)
+        hist_dict['g'] = cv2.calcHist([img], [1], None, [256], [0, 256]).reshape(-1)
+        hist_dict['b'] = cv2.calcHist([img], [2], None, [256], [0, 256]).reshape(-1)
+    return hist_dict
