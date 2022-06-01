237c15c9fb37f64484383d920f8fafa26d14f0ac
ZEH0 <1335819001@qq.com>
2022-06-01 16:15:21 +0800

    feat[均衡化三通道]

--- api/views.py
+++ api/views.py
@@ -113,17 +113,11 @@ class linearChange(APIView):  # 分段线性变化
         dict['filepath'] = path
         # inputA、inputB、inputC、inputD
         # a、b、c、d
-<<<<<<< HEAD
         dict['a'] = int(request.data.get('inputA'))
         dict['b'] = int(request.data.get('inputB'))
         dict['c'] = int(request.data.get('inputC'))
         dict['d'] = int(request.data.get('inputD'))
-=======
-        dict['a'] = request.data.get('inputA')
-        dict['b'] = request.data.get('inputB')
-        dict['c'] = request.data.get('inputC')
-        dict['d'] = request.data.get('inputD')
->>>>>>> PapriKey/dmp_course-project/master
+
 
         opera('gray_three_linear_trans', dict)
         # 以下也是复制粘贴
@@ -269,11 +263,7 @@ class gammaChange(APIView):  # 幂次变换
         # gamma
         dict = {}
         dict['filepath'] = path
-<<<<<<< HEAD
         dict['gamma'] = eval(request.data.get('inputGamma'))
-=======
-        dict['gamma'] = request.data.get('inputGamma')
->>>>>>> PapriKey/dmp_course-project/master
 
         opera('gamma', dict)
         # 以下也是复制粘贴
@@ -341,21 +331,11 @@ class addSaltPepper(APIView):  # 椒盐噪声
 
         # 调用处理函数
         # zoomPepperValue、zoomSaltValue
-<<<<<<< HEAD
         # pa、pb
         dict = {}
         dict['filepath'] = path
         dict['pa'] = request.data.get('zoomPepperValue')
         dict['pb'] = request.data.get('zoomSaltValue')
-=======
-        dict = {}
-        dict['filepath'] = path
-        # dict['a']=request.data.get('a')
-        # dict['b']=request.data.get('b')
-        # dict['c']=request.data.get('c')
-        # dict['d']=request.data.get('d')
->>>>>>> PapriKey/dmp_course-project/master
-
         opera('salt_pepper_noise', dict)
         # 以下也是复制粘贴
         # 返回定制格式的JSON
@@ -517,14 +497,17 @@ class filter(APIView):  # 平滑滤波（中值/均值）
         # 以上为复制粘贴操作
 
         # 调用处理函数
+        # ValueOfMeanOrMedian、inputMeanOrMedianSize
+        # op_name、ksize
         dict = {}
         dict['filepath'] = path
-        # dict['a']=request.data.get('a')
-        # dict['b']=request.data.get('b')
-        # dict['c']=request.data.get('c')
-        # dict['d']=request.data.get('d')
+        dict['op_name'] = request.data.get('ValueOfMeanOrMedian')
+        temp= int(request.data.get('inputMeanOrMedianSize'))
+        if(temp%2==0):
+            temp+=1
+        dict['ksize'] = temp
 
-        opera('median_blur', dict)
+        opera('filter', dict)
         # 以下也是复制粘贴
         # 返回定制格式的JSON
         return success(serializer.data)
--- lib/manage/imageProcess.py
+++ lib/manage/imageProcess.py
@@ -99,7 +99,13 @@ def gamma(img, gamma=2., eps=0.):
     return 255. * ((img + eps) / 255.) ** gamma
 
 def hist_equal(img):
-    return cv2.equalizeHist(img)
+    if img.ndim==2:
+        return cv2.equalizeHist(img)
+    elif img.ndim==3:
+        b=cv2.equalizeHist(img[:,:,0])
+        g=cv2.equalizeHist(img[:,:,1])
+        r=cv2.equalizeHist(img[:,:,2])
+        return cv2.merge([b,g,r])
 
 # 分段线性变换
 def gray_three_linear_trans(input, a, b, c=0, d=255):
@@ -179,9 +185,16 @@ def mean_blur(img, ksize):
     '''
     return cv2.blur(img, (ksize, ksize))
 
-
 def median_blur(img, ksize):
     return cv2.medianBlur(img, ksize)
+
+def filter(img,op_name,ksize):
+    if op_name == 'mean':
+        return mean_blur(img,ksize)
+    elif op_name == 'median':
+        return median_blur(img,ksize)
+    else:
+        return img
 
 
 # 自适应局部降噪
